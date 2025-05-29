05096b5d9e95bdf4bc5748d421c3115c73f9b96a
Kim Sojung <166891903+sojungsojung@users.noreply.github.com>
2025-05-29 17:48:34 +0900

    Update run_saige_gene.R

--- R/run_saige_gene.R
+++ R/run_saige_gene.R
@@ -1,31 +1,44 @@
 #' Run SAIGE-GENE+ on a phenotype
 #'
-#' @param phenoFile Path to phenotype file
-#' @param genotypeFiles Character vector of genotype file paths (BGEN/PLINK)
-#' @param groupFile Path to group file
+#' @param phenoFile Path to phenotype file (tab-delimited)
+#' @param genotypeFiles Character vector of genotype file paths (BGEN or PLINK)
+#' @param groupFile Path to gene-set (group) file
 #' @param outputDir Directory to store SAIGE output
 #' @return data.table with columns Gene, p.value
 #' @export
-run_saige_gene <- function(phenoFile, genotypeFiles, groupFile, outputDir) {
+run_saige_gene <- function(phenoFile,
+                           genotypeFiles,
+                           groupFile,
+                           outputDir) {
   dir.create(outputDir, recursive = TRUE, showWarnings = FALSE)
+
+  # build and run the SAIGE step2 command
   cmd <- sprintf(
-    "Rscript %s/step2_SPAtests.R \
-      --phenoFile=%s \
-      --plinkFile=%s \
-      --groupFile=%s \
-      --outputPrefix=%s/saige_out \
-      --nThreads=4 \
-      --LOCO=FALSE",
-    system.file("extdata", package="SAIGE"),
+    "docker run -v /data:/data wzhou88/saige:1.4.4 step2_SPAtests.R \
+     --phenoFile=%s \
+     --plinkFile=%s \
+     --groupFile=%s \
+     --outputPrefix=%s/saige_out \
+     --nThreads=4 \
+     --LOCO=FALSE",
     phenoFile,
-    paste(genotypeFiles, collapse=","),
+    paste(genotypeFiles, collapse = ","),
     groupFile,
     outputDir
   )
   status <- system(cmd)
-  if (status != 0) stop("SAIGE-GENE+ failed with status ", status)
-  out_file <- file.path(outputDir, "saige_out.html")  # adjust to actual merged.out path
+  if (status != 0) {
+    stop("SAIGE-GENE+ failed with exit status ", status)
+  }
+
+  # SAIGE step2 produces <outputPrefix>.SAIGE_GENE_output.txt
+  out_file <- file.path(outputDir, "saige_out.SAIGE_GENE_output.txt")
+  if (!file.exists(out_file)) {
+    stop("Cannot find SAIGE output at ", out_file)
+  }
+
+  # read and return
   res <- data.table::fread(out_file)
-  setnames(res, c("Gene","p.value"))
+  data.table::setnames(res, c("Gene", "p.value"))
   return(res)
 }
